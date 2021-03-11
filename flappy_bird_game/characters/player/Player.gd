165e9495bca93b1f0481c67c20928d8e973cdf42
Sajawal Hassan <scientificninja1f12@gmail.com>
2021-03-11 17:23:20 +0500

    added the animation for the player and added the ground with animation aswell

--- flappy_bird_game/characters/player/Player.gd
+++ flappy_bird_game/characters/player/Player.gd
@@ -2,6 +2,8 @@ extends RigidBody2D
 
 export var FLAP_FORCE = -300
 
+const MAX_ROTATION_DEGREE = -40
+
 onready var animator = $AnimationPlayer
 
 var started = false
@@ -11,6 +13,16 @@ func _physics_process(delta) -> void:
 		if !started:
 			start()
 		flap()
+		
+	if rotation_degrees <= MAX_ROTATION_DEGREE:
+		angular_velocity = 0
+		rotation_degrees = MAX_ROTATION_DEGREE
+		
+	if linear_velocity.y > 0:
+#		if rotation_degrees <= 90:
+#			angular_velocity = 3
+		if linear_velocity.y > 0:
+			angular_velocity = 1
 	
 func start():
 	if started: return
@@ -20,3 +32,4 @@ func start():
 
 func flap():
 	linear_velocity.y = FLAP_FORCE
+	angular_velocity = -8
